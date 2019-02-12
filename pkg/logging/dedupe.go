7a4ed0df7cc449000af2251d5be26fada0975e82
Tom Wilkie <tom.wilkie@gmail.com>
2019-02-12 16:41:51 +0000

    Add dedupe test.
    Signed-off-by: Tom Wilkie <tom.wilkie@gmail.com>

--- pkg/logging/dedupe.go
+++ pkg/logging/dedupe.go
@@ -9,22 +9,10 @@ import (
 	"github.com/go-logfmt/logfmt"
 )
 
-type deduper struct {
-	next   log.Logger
-	repeat time.Duration
-
-	mtx  sync.RWMutex
-	seen map[string]time.Time
-}
-
-// Dedupe log lines to next, only repeating every repeat duration.
-func Dedupe(next log.Logger, repeat time.Duration) log.Logger {
-	return &deduper{
-		next:   next,
-		repeat: repeat,
-		seen:   map[string]time.Time{},
-	}
-}
+const (
+	garbageCollectEvery = 10 * time.Second
+	expireEntriesAfter  = 1 * time.Minute
+)
 
 type logfmtEncoder struct {
 	*logfmt.Encoder
@@ -39,24 +27,55 @@ var logfmtEncoderPool = sync.Pool{
 	},
 }
 
-func encode(keyvals ...interface{}) (string, error) {
-	enc := logfmtEncoderPool.Get().(*logfmtEncoder)
-	enc.Reset()
-	defer logfmtEncoderPool.Put(enc)
-
-	if err := enc.EncodeKeyvals(keyvals...); err != nil {
-		return "", err
-	}
-
-	// Add newline to the end of the buffer
-	if err := enc.EndRecord(); err != nil {
-		return "", err
-	}
-
-	return string(enc.buf.Bytes()), nil
+// Deduper implement log.Logger, dedupes log lines.
+type Deduper struct {
+	next   log.Logger
+	repeat time.Duration
+	quit   chan struct{}
+	mtx    sync.RWMutex
+	seen   map[string]time.Time
 }
 
-func (d *deduper) Log(keyvals ...interface{}) error {
+// Dedupe log lines to next, only repeating every repeat duration.
+func Dedupe(next log.Logger, repeat time.Duration) *Deduper {
+	d := &Deduper{
+		next:   next,
+		repeat: repeat,
+		quit:   make(chan struct{}),
+		seen:   map[string]time.Time{},
+	}
+	go d.run()
+	return d
+}
+
+// Stop the Deduper.
+func (d *Deduper) Stop() {
+	close(d.quit)
+}
+
+func (d *Deduper) run() {
+	ticker := time.NewTicker(garbageCollectEvery)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			d.mtx.Lock()
+			now := time.Now()
+			for line, seen := range d.seen {
+				if now.Sub(seen) > expireEntriesAfter {
+					delete(d.seen, line)
+				}
+			}
+			d.mtx.Unlock()
+		case <-d.quit:
+			return
+		}
+	}
+}
+
+// Log implements log.Logger.
+func (d *Deduper) Log(keyvals ...interface{}) error {
 	line, err := encode(keyvals...)
 	if err != nil {
 		return err
@@ -75,4 +94,21 @@ func (d *deduper) Log(keyvals ...interface{}) error {
 	d.mtx.Unlock()
 
 	return d.next.Log(keyvals...)
+}
+
+func encode(keyvals ...interface{}) (string, error) {
+	enc := logfmtEncoderPool.Get().(*logfmtEncoder)
+	enc.buf.Reset()
+	defer logfmtEncoderPool.Put(enc)
+
+	if err := enc.EncodeKeyvals(keyvals...); err != nil {
+		return "", err
+	}
+
+	// Add newline to the end of the buffer
+	if err := enc.EndRecord(); err != nil {
+		return "", err
+	}
+
+	return string(enc.buf.Bytes()), nil
 }
